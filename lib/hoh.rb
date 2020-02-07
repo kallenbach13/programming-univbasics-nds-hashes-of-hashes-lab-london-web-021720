a6d66bb43327b8ba552f87eaa6342f6698a9fe88
Isak Kallenbach <kallenbach13@gmail.com>
2020-02-07 14:44:56 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -47,13 +47,13 @@ def naming_system
      label: "Phylum",
       sub_category: {
        label: "Class"
-    }
-   }
+    },
+   },
   label: "Order",
   label: "Family",
-   sub_category: {
+  sub_category: {
      label: "Genus",
-   }
+   },
    label: "Species",
    sub_category: nil
   }
